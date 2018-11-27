72b143d25dca23b242ca7f9a19442090a2c7af95
tushartripathi1998 <thetushartripathi1998@gmail.com>
2018-11-27 22:02:00 +0530

    Added 'beforeEach'

--- test/cls.test.js
+++ test/cls.test.js
@@ -9,13 +9,17 @@ class Car{
 		}
 }
 
+let car;
+
+beforeEach(()=>{
+car = new Car();
+});
+
 describe('Car action', () =>{
 	it('car park',() =>{
-		const car = new Car();
 		assert.equal(car.park(), 'stopped');
 });
 	it('car driven',() =>{
-		const car = new Car();
 		assert.equal(car.drive(), 'vroom');
 });
 });
